e0f8a29faa55dc2674d60d43feb9c90aef9185c5
Your Name <you@example.com>
2017-07-26 00:22:37 +0300

    Remove Class Remove Test

--- src/main/java/ItBooksTests.java
+++ src/main/java/ItBooksTests.java
@@ -87,10 +87,7 @@ public class ItBooksTests {
         webSite.searchResultsPage().allPagesAreAvailable("Test iOS Apps with UI Automation");
         webSite.searchResultsPage().allPagesAreAvailable("Windows Server 2012 Automation with PowerShell Cookbook");
     }
-    @Test
-    public void testForGit(){
 
-    }
 
         @After
     public void tearDown(){
